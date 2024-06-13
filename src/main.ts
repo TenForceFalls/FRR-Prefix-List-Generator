c5646317248d72a6164eab80bdb3cbdb3d344d22
big dick bandit <dylan@tritan.gg>
2024-06-13 14:34:33 -0400

    lil cleaner

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import extractASNs from "./lib/extractASNs";
 import generatePrefixLists from "./lib/generatePrefixLists";
 import { execSync } from "child_process";
 
-let v4Done = false;
-let v6Done = false;
+let done = false;
 
 async function main() {
   let asns = extractASNs();
@@ -13,26 +12,19 @@ async function main() {
     const asSets = await fetchAsSets(`${asn}`);
     const prefixLists = generatePrefixLists(`${asn}`, asSets);
 
-    prefixLists.v4.forEach((ctx) => {
-      execSync(`vtysh -c "conf t" -c "${ctx}" -c "end" -c "exit"`);
-      console.log(`Adding ${ctx}`);
-    });
+    const combinedPrefixLists = [...prefixLists.v4, ...prefixLists.v6];
 
-    prefixLists.v6.forEach((ctx) => {
+    combinedPrefixLists.forEach((ctx) => {
       execSync(`vtysh -c "conf t" -c "${ctx}" -c "end" -c "exit"`);
       console.log(`Adding ${ctx}`);
     });
   }
 
-  v4Done = true;
-  v6Done = true;
+  done = true;
 }
 
 main().catch((error) => {
   console.error("An error occurred:", error);
 
-  if (v4Done) console.log("v4 prefixes added successfully");
-  if (v6Done) console.log("v6 prefixes added successfully");
-
-  if (v4Done && v6Done) process.exit(1);
+  if (done) process.exit(1);
 });

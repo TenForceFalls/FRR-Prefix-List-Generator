a4486785037c37cdc72b80d7587d66ad347ce62e
big dick bandit <dylan@tritan.gg>
2024-06-13 14:18:53 -0400

    ok done!

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import extractASNs from "./lib/extractASNs";
 import generatePrefixLists from "./lib/generatePrefixLists";
 import { execSync } from "child_process";
 
+let v4Done = false;
+let v6Done = false;
+
 async function main() {
   let asns = extractASNs();
 
@@ -20,8 +23,16 @@ async function main() {
       console.log(`Adding ${ctx}`);
     });
   }
+
+  v4Done = true;
+  v6Done = true;
 }
 
 main().catch((error) => {
   console.error("An error occurred:", error);
+
+  if (v4Done) console.log("v4 prefixes added successfully");
+  if (v6Done) console.log("v6 prefixes added successfully");
+
+  if (v4Done && v6Done) process.exit(1);
 });

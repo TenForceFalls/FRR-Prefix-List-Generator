b65b5e06411b4b49a75c6d5fb5e2c0a370821308
big dick bandit <dylan@tritan.gg>
2024-06-13 14:04:08 -0400

    weird recursion error

--- src/lib/generatePrefixLists.ts
+++ src/lib/generatePrefixLists.ts
@@ -18,8 +18,16 @@ function generatePrefixLists(asn: string, asSets: string[]): PrefixLists {
     const resultIPv4 = execSync(bgpq4IPv4Command, { encoding: "utf-8" });
     const resultIPv6 = execSync(bgpq4IPv6Command, { encoding: "utf-8" });
 
-    results.v4.push(...resultIPv4.trim().split("\n"));
-    results.v6.push(...resultIPv6.trim().split("\n"));
+    const linesIPv4 = resultIPv4.trim().split("\n");
+    const linesIPv6 = resultIPv6.trim().split("\n");
+
+    for (let i = 0; i < linesIPv4.length; i++) {
+      results.v4.push(linesIPv4[i]);
+    }
+
+    for (let i = 0; i < linesIPv6.length; i++) {
+      results.v6.push(linesIPv6[i]);
+    }
   });
 
   return results;
